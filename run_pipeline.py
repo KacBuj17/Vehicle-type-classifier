856b508aeb32866c377b59ecd6586a0b0a6153fe
Kacper Bujak <kbujak150@interia.pl>
2025-11-27 19:37:50 +0100

    run_pipelien script corrected

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,9 +1,12 @@
 import os
+import sys
 import subprocess
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 SOURCE = os.path.abspath(os.path.join(ROOT, "source"))
 SCRIPTS = os.path.join(ROOT, "scripts")
+
+python_exe = sys.executable
 
 env = os.environ.copy()
 env["PYTHONPATH"] = SOURCE
@@ -12,7 +15,7 @@ def run_script(name):
     script_path = os.path.join(SCRIPTS, name)
     print(f"\nRunning {script_path}")
     subprocess.run(
-        ["python", script_path],
+        [python_exe, script_path],
         check=True,
         env=env
     )
